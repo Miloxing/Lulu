71ade5619d03cf6d2d339dc3a770085b3305940f
Zongqian Zhang <1650658858@qq.com>
2018-08-28 22:48:39 +0800

    文件名增加房间名

--- lulu/extractors/panda.py
+++ lulu/extractors/panda.py
@@ -40,7 +40,7 @@ def panda_download(url, info_only=False, **kwargs):
     
     sTime = time.strftime('%y%m%d_%H%M%S')
     
-    title = sTime+'-'+data['roominfo']['name']
+    title = sTime+'-'+data['hostinfo']['name']+'-'+data['roominfo']['name']
     room_key = data['videoinfo']['room_key']
     plflag = data['videoinfo']['plflag'].split('_')
     status = data['videoinfo']['status']
@@ -61,7 +61,7 @@ def panda_download(url, info_only=False, **kwargs):
     if not info_only:
         download_urls([real_url], title, 'flv', None, **kwargs)
         time.sleep(5)
-        os.system('rclone move /root/b/d/"{}.mp4" milo:milo/b/"{}"'.format(title,data['name']))
+        os.system('rclone move /root/b/d/"{}.flv" milo:milo/b/"{}"'.format(title,data['hostinfo']['name']))
 
 
 download = panda_download
