901c71cbfec6318f8282d05870af5892bb18e347
Zongqian Zhang <1650658858@qq.com>
2018-11-27 14:12:06 +0800

    Update common.py

--- lulu/common.py
+++ lulu/common.py
@@ -284,10 +284,7 @@ def post_content(url, headers=FAKE_HEADERS, post_data={}):
 
 def url_size(url, headers=FAKE_HEADERS):
     response = urlopen_with_retry(url, headers=headers)
-    try:
-        size = response.headers['content-length']
-    except KeyError as e:
-        size = None
+    size = response.headers.get('content-length',None)
     return int(size) if size is not None else float('inf')
 
 
